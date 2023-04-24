0b61ba14ed7c2b0c21fcbe6fd13319eaa127870d
Nicholas Davolt <nicholadavolt@outlook.com>
2023-04-24 15:29:48 -0500

    Added initial menu items and loops

---
+++ Dungeon/DungeonMenu.cs
@@ -0,0 +1,63 @@
+﻿namespace Dungeon
+{
+    internal class DungeonMenu
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dungeon App");
+
+            //TODO Create a Player
+
+            bool mainLoop = true; //counter for the main game loop
+
+            do
+            {
+                //TODO Create a Monster
+                //TODO Create a Room
+
+                bool innerMenu = true; //counter for inner menu
+
+                do
+                {
+                    Console.WriteLine("\n\nA. Attack\n" +
+                        "B. Run Away\n" +
+                        "C. Character Info\n" +
+                        "D. Monster Info\n" +
+                        "E. Exit\n");
+                    string userSelection = Console.ReadLine().ToUpper();
+                    Console.Clear();
+
+                    switch (userSelection)
+                    {
+                        case "A":
+                            Console.WriteLine("Attack");
+                            //WIN innerMenu = false;
+                            //Lose innerMenu = false; && mainLoop = false;
+                            break;
+                        case "B":
+                            Console.WriteLine("Run Away");
+                            innerMenu = false;
+                            break;
+                        case "C":
+                            Console.WriteLine("Character Info");
+                            //TODO Character Info
+                            break;
+                        case "D":
+                            Console.WriteLine("Monster Info");
+                            //TODO Monster Info
+                            break;
+                        case "E":
+                            Console.WriteLine("Thanks for playing!");
+                            innerMenu=false;
+                            mainLoop = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid selection, try again");
+                            break;
+                    }
+                } while (innerMenu);
+            } while (mainLoop);
+        }
+    }
+}
