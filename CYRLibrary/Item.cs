6ae4e7db324c7b65ff10ca153f90bef52bd97303
Nicholas Davolt <nicholadavolt@outlook.com>
2023-05-01 17:31:41 -0500

    added various classes to the CYR library.

---
+++ CYRLibrary/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CYRLibrary
+{
+    public abstract class Item
+    {
+        public string Name { get; set; }
+        public bool IsConsumable { get; set; }
+
+
+        public Item(string name, bool isConsumable)
+        {
+            Name = name;
+            IsConsumable = isConsumable;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\n";
+        }
+    }
+}
---
+++ Dungeon/TestHarness.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CYRLibrary;
+
+namespace Dungeon
+{
+    internal class TestHarness
+    {
+        static void Main(string[] args)
+        {
+            Weapon w1 = new Weapon("Broom", false, 8, 5, 1, 1, WeaponType.Stick);
+
+            Console.WriteLine(w1);
+
+            //Equipment_Head e1 = new Equipment_Head("Trashcan Lid", false, 0, 10, 0, EquipLocations.hands,1);
+
+            //Console.WriteLine(e1);
+        }
+
+    }
+}
